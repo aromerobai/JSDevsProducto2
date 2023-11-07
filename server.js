f66eb6bf8a052a050b9aa2d2fac67aee6dbe7e93
aromerobai <aromerobai@uoc.edu>
2023-11-07 17:38:06 +0100

    Cambios en server.js

--- server.js
+++ server.js
@@ -3,16 +3,35 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, req.url);
-  const fileStream = fs.createReadStream(filePath);
+  const url = req.url === '/' ? '/index.html' : req.url; // Redirige la URL raíz a index.html
 
-  fileStream.on('error', (err) => {
+  let filePath;
+  let contentType;
+
+  if (url.endsWith('.html')) {
+    contentType = 'text/html';
+    filePath = path.join(__dirname, 'html', url);
+  } else if (url.endsWith('.js')) {
+    contentType = 'text/javascript';
+    filePath = path.join(__dirname, 'js', url);
+  } else if (url.endsWith('.css')) {
+    contentType = 'text/css';
+    filePath = path.join(__dirname, 'css', url);
+  } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('File not found');
-  });
+    return;
+  }
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  fileStream.pipe(res);
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
 });
 
 const port = 3000;
